bda2caa9bbcc05bcdfa5dc83d9124e7afc6a9280
Michael Wagner <wagner.mchl@googlemail.com>
2019-08-26 23:46:46 +0800

    fixed: comment punctuation color not in line with comment content

--- deploy_local.sh
+++ deploy_local.sh
@@ -1,5 +1,5 @@
 echo 'deploying highland meltwater locally'
 
-# rm -r  ~/.vscode/extensions/highland-meltwater-syntax
+# rm -rf  ~/.vscode/extensions/highland-meltwater-syntax
 
 cp -r ../highland-meltwater-syntax ~/.vscode/extensions
